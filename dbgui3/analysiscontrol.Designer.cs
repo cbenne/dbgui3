3d0c37b1afb54ae04ff254ccf6d4126f428e8b42
laurenkl96 <lklynch@ksu.edu>
2017-11-30 19:42:35 -0600

    added analysis control to form

--- dbgui3/analysiscontrol.Designer.cs
+++ dbgui3/analysiscontrol.Designer.cs
@@ -28,18 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.basicBar = new LiveCharts.WinForms.CartesianChart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // basicBar
+            // 
+            this.basicBar.Location = new System.Drawing.Point(435, 179);
+            this.basicBar.Name = "basicBar";
+            this.basicBar.Size = new System.Drawing.Size(1029, 588);
+            this.basicBar.TabIndex = 0;
+            this.basicBar.Text = "PartsSoldPurchased";
+            this.basicBar.ChildChanged += new System.EventHandler<System.Windows.Forms.Integration.ChildChangedEventArgs>(this.cartesianChart1_ChildChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(554, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(774, 36);
+            this.textBox1.TabIndex = 1;
             // 
             // analysiscontrol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.basicBar);
             this.Name = "analysiscontrol";
             this.Size = new System.Drawing.Size(1499, 793);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private LiveCharts.WinForms.CartesianChart basicBar;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
